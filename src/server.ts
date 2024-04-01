d5a3a4178522f0abbc332628b24129be731bc073
Beatriz <beatriz.lhopes@alcans.com.br>
2024-03-31 22:47:57 -0300

    Aula variaveis de ambiente

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,18 @@
 import fastify from 'fastify'
 import { knex } from './database'
+import crypto from 'node:crypto'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
-
-  return tables
+  const transactions = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'Transação de teste',
+      amount: 1000,
+    })
+    .returning('*')
+  return transactions
 })
 
 app
